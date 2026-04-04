938031955e5eefbd325e4f32b98ab9ad484edf4e
Mateus Diniz <serv.mateus@gmail.com>
2026-04-04 01:56:07 -0300

    feat: acesso a endpoints de criação e edição.

--- goledger-challenge-web/src/services/apiServices.js
+++ goledger-challenge-web/src/services/apiServices.js
@@ -16,13 +16,100 @@ export async function getSchemas(){
 export async function getShowsList(){
 
     const payload = {
-    query: {
-      selector: {
+    "query": {
+      "selector": {
         "@assetType": "tvShows"
       }
     }
   };
 
   const response = await api.post("/query/search", payload);
-    return response.data;
+    return response.data.result;
+}
+
+
+export async function getSearchedShow(id){
+
+    const payload = {
+    "query": {
+      "selector": {
+        "@assetType": "tvShows",
+        "@key": id
+      }
+    }
+  };
+
+  const response = await api.post("/query/search", payload);
+    return response.data.result;
+}
+
+
+export async function getSeasonsShow(key){
+
+    const payload = {
+      "query": {
+        "selector": {
+          "@assetType": "seasons",
+          "tvShow": {
+            "@assetType": "tvShows",
+            "@key": key
+          }
+        }
+      }
+    };
+
+  const response = await api.post("/query/search", payload);
+    return response.data.result;
+}
+
+export async function getEpisodesSeason(key){
+
+    const payload = {
+    "query": {
+      "selector": {
+        "@assetType": "episodes",
+        "season": {
+        "@assetType": "seasons",
+        "@key": key
+      }
+      }
+    }
+  };
+
+  const response = await api.post("/query/search", payload);
+    return response.data.result;
+}
+
+export async function getDetailSeason(key){
+
+    const payload = {
+      "query": {
+        "selector": {
+          "@assetType": "episodes",
+          "season": {
+            "@assetType": "seasons",
+            "@key": key
+          }
+        }
+      }
+    };
+
+  const response = await api.post("/query/search", payload);
+    return response.data.result;
+}
+
+export async function postCreateAsset(attr){
+  
+  const payload = JSON.stringify({
+    "asset": [attr]
+  })
+  await api.post("/invoke/createAsset", payload)
+}
+
+export async function putUpdateAsset(attr){
+  const payload = JSON.stringify({
+    "update": attr
+  })
+  console.log(payload)
+  await api.put("/invoke/updateAsset", payload)
 }
