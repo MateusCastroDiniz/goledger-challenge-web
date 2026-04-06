9fce1ebdc59ec9cd2266787ebf92b4d86cc6c46c
Mateus Diniz <serv.mateus@gmail.com>
2026-04-06 03:52:14 -0300

    feat: funcionalidade de busca de título

--- goledger-challenge-web/src/hooks/tvShow/useShows.js
+++ goledger-challenge-web/src/hooks/tvShow/useShows.js
@@ -1,56 +1,95 @@
 import { useEffect, useState, useCallback } from 'react'
-import { getShowsList } from '../../services/apiServices'
+import { getShowsList, getSearchedShow } from '../../services/apiServices'
 
 export default function useShows() {
+
     const [shows, setShows] = useState([])
     const [loading, setLoading] = useState(false)
     const [searchTerm, setSearchTerm] = useState("")
     const [currentPage, setCurrentPage] = useState(1)
     const [refresh, setRefresh] = useState(0)
-    
+
     const [bookmarks, setBookmarks] = useState({ 1: "" })
     const [hasMore, setHasMore] = useState(true)
 
     const fetchShows = useCallback(async (pageNumber) => {
+
         setLoading(true)
-        try {
-            const bookmarkToUse = bookmarks[pageNumber] || ""
-            const data = await getShowsList(bookmarkToUse)
-            
-            const results = Array.isArray(data?.result) ? data.result : []
-            setShows(results)
 
-            const nextBookmark = data.metadata?.bookmark;
+        if (searchTerm) {
 
-            if (nextBookmark) {
-                setBookmarks(prev => ({
-                    ...prev,
-                    [pageNumber + 1]: nextBookmark
-                }))
-                // Se retornou 10 registros, provavelmente tem mais
-                setHasMore(data.metadata.fetched_records_count == results.length)
-            } else {
+            try {
+                const responseSearch = await getSearchedShow("", searchTerm)
+
+                setShows(Array.isArray(responseSearch) ? responseSearch : [])
                 setHasMore(false)
-            }
-        } catch (err) {
-            console.error("Erro ao buscar shows:", err)
-            setShows([])
-            setHasMore(false)
-        } finally {
-            setLoading(false)
-        }
-    }, [bookmarks])
 
-    // Reset quando muda busca ou refresh
+            } catch (err) {
+
+                console.error("Erro na busca:", err)
+                setShows([])
+
+            } finally {
+
+                setLoading(false)
+
+            }
+
+        } else {
+
+            try {
+
+                const bookmarkToUse = bookmarks[pageNumber] || ""
+
+                const data = await getShowsList(bookmarkToUse)
+
+                const results = Array.isArray(data?.result) ? data.result : []
+
+                setShows(results)
+
+                const nextBookmark = data.metadata?.bookmark
+
+                if (nextBookmark) {
+
+                    setBookmarks(prev => ({
+                        ...prev,
+                        [pageNumber + 1]: nextBookmark
+                    }))
+
+                    setHasMore(results.length === 6)
+
+                } else {
+
+                    setHasMore(false)
+
+                }
+
+            } catch (err) {
+
+                console.error("Erro ao buscar shows:", err)
+                setShows([])
+                setHasMore(false)
+
+            } finally {
+
+                setLoading(false)
+
+            }
+        }
+
+    }, [bookmarks, searchTerm])
+
     useEffect(() => {
+
         setBookmarks({ 1: "" })
         setCurrentPage(1)
-        // O useEffect debaixo cuidará da chamada inicial
+
     }, [searchTerm, refresh])
 
-    // Dispara a busca quando a página muda
     useEffect(() => {
+
         fetchShows(currentPage)
+
     }, [currentPage, refresh, searchTerm])
 
     const handlePageChange = (event, newPage) => {
